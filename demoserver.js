cc368e44c19612e7ff011f9a42601239ddda10c6
anarres <aanarres@gmail.com>
2016-07-07 13:34:30 +0200

    demoserver now sends out our own response

--- demoserver.js
+++ demoserver.js
@@ -4,14 +4,14 @@
 
 var http = require('http');
 
-// in the following, the server returned by createServer is an EventEmitter
-// we attach a function that is executed every request directly or separately
-// separately:
-// var server = http.createServer(); // this EventEmitter takes a listener
-// server.on('request', function(request, response) {
+//in the following, the server returned by createServer is an EventEmitter
+//we attach a function that is executed every request directly or separately
+//separately:
+//var server = http.createServer(); // this EventEmitter takes a listener
+//server.on('request', function(request, response) {
 
-// directly:
-// giving the EventEmitter a listener on each request
+//directly:
+//giving the EventEmitter a listener on each request
 var server = http.createServer(function(request, response) {
 	// note we have the age-old request and response pair
 	// request is a ReadableStream
@@ -22,54 +22,41 @@ var server = http.createServer(function(request, response) {
 
 	request.on('error', function(err) {
 		console.error(err);
-	}).on('data', function(chunk) {
-		body.push(chunk);
-	}).on('end', function() {
-		body = Buffer.concat(body).toString();
+	}) // .on('end', function() {
 
-		// lets deal with response too, cause that is the whole purpose here
-		// response is a WriteableStream, so we have write(), end(), on() for listeners such as for errors thrown
-		response.on('error', function(err) {
-			console.error(err);
-		});
+	body = "There's nothing wrong with your server. We now control the communication.";
+	
+	// use our new String outside its defining function:
+	var specialMessage = "We now control the communication.";
+	console.log(String.prototype.fromString.call(specialMessage, specialMessage)); // call it with an object
 
-		// the following can also be written as:
-		// response.writeHead(200, {
-		// 'Content-Type': 'application/json',
-		// });
+	// lets deal with response too, cause that is the whole purpose here
+	// response is a WriteableStream, so we have write(), end(), on() for listeners such as for errors thrown
+	response.on('error', function(err) {
+		console.error(err);
+	});
 
-		response.statusCode = 200;
-		response.setHeader('Content-Type', 'application/json');
+	response.statusCode = 200;
+	response.setHeader('Content-Type', 'application/json');
 
-		var responseBody = {
+	var responseBody = {
 			headers : headers,
 			method : method,
 			url : url,
 			body : body
-		};
+	};
 
-		response.write(JSON.stringify(responseBody));
-		response.end();
-		// Note: the 2 lines above could be replaced with this next one:
-		// response.end(JSON.stringify(responseBody))
-	});
+	response.write(body);
+	response.end();
+	// Note: the 2 lines above could be replaced with this next one:
+	// response.end(JSON.stringify(responseBody))
 }).listen(8008, '127.0.0.1', function() {
-	randomFunc();
 	
-	// use our new String outside its defining function:
-	var myStr = "Amsterdam is a beautiful city in Northern Europe.";
-	console.log(String.prototype.fromString.apply(myStr, [myStr])); // call it with an array
-}); // begin accepting connections
-
-function randomFunc() {
 	// add a named function to an object prototype
+	// just because we can (@todo removeme and all that)
 	String.prototype.fromString = function(from) {
 		return from + 'Composed String.';
 	};
-	
-	var myStr = "Amsterdam is a city in Northern Europe.";
-	console.log(myStr.fromString(myStr));
-	
-	// calls via reflection ftw
-	console.log(String.prototype.fromString.call(myStr, myStr)); // with an object
-}
+
+}); // begin accepting connections
+
