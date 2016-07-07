725330d43315507765cb109732de3dd8abd66a64
anarres <aanarres@gmail.com>
2016-07-07 11:20:26 +0200

    fix source format

--- demoserver.js
+++ demoserver.js
@@ -13,44 +13,44 @@ var http = require('http');
 // directly:
 // giving the EventEmitter a listener on each request
 var server = http.createServer(function(request, response) {
-  // note we have the age-old request and response pair
-  // request is a ReadableStream
-  var headers = request.headers;
-  var method = request.method;
-  var url = request.url;
-  var body = [];
+	// note we have the age-old request and response pair
+	// request is a ReadableStream
+	var headers = request.headers;
+	var method = request.method;
+	var url = request.url;
+	var body = [];
 
-  request.on('error', function(err) {
-    console.error(err);
-  }).on('data', function(chunk) {
-    body.push(chunk);
-  }).on('end', function() {
-    body = Buffer.concat(body).toString();
-    
-    // lets deal with response too, cause that is the whole purpose here
-    // response is a WriteableStream, so we have write(), end(), on() for listeners such as for errors thrown
-    response.on('error', function(err) {
-      console.error(err);
-    });
+	request.on('error', function(err) {
+		console.error(err);
+	}).on('data', function(chunk) {
+		body.push(chunk);
+	}).on('end', function() {
+		body = Buffer.concat(body).toString();
 
-    // the following can also be written as:
-    // response.writeHead(200, {
-    // 'Content-Type': 'application/json',
-    // });
-    
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'application/json');
- 
-    var responseBody = {
-      headers: headers,
-      method: method,
-      url: url,
-      body: body
-    };
+		// lets deal with response too, cause that is the whole purpose here
+		// response is a WriteableStream, so we have write(), end(), on() for listeners such as for errors thrown
+		response.on('error', function(err) {
+			console.error(err);
+		});
 
-    response.write(JSON.stringify(responseBody));
-    response.end();
-    // Note: the 2 lines above could be replaced with this next one:
-    // response.end(JSON.stringify(responseBody))
-  });
+		// the following can also be written as:
+		// response.writeHead(200, {
+		// 'Content-Type': 'application/json',
+		// });
+
+		response.statusCode = 200;
+		response.setHeader('Content-Type', 'application/json');
+
+		var responseBody = {
+			headers : headers,
+			method : method,
+			url : url,
+			body : body
+		};
+
+		response.write(JSON.stringify(responseBody));
+		response.end();
+		// Note: the 2 lines above could be replaced with this next one:
+		// response.end(JSON.stringify(responseBody))
+	});
 }).listen(8008); // begin accepting connections
